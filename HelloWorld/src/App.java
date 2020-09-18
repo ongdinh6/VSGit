0d5eb18340999859790a3b27ec7d6fa850867885
ongdinh6 <62459698+ongdinh6@users.noreply.github.com>
2020-09-18 19:40:38 +0700

    Update App.java

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("This is first code!");
+        System.out.println("This is first code in visual studio with using git!");
+       
     }
 }
